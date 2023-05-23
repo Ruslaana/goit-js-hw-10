bcc159964988bd1ec7dc100ce38783cee2bdf599
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-05-23 12:30:02 +0200

    task 10

---
+++ src/fetchCountries/fetchCountries.js
@@ -0,0 +1,15 @@
+const URL_API = 'https://restcountries.com/v3.1/name';
+
+function fetchCountriesData(name) {
+  return fetch(
+    `${URL_API}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error('Country not found');
+    }
+
+    return response.json();
+  });
+}
+
+export default fetchCountriesData;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+import { fetchCountriesData } from './fetchCountries/fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const countryInput = document.getElementById('search-box');
+const countryList = document.getElementById('country-list');
+
+function displayCountries(countries) {
+  countryList.innerHTML = '';
+
+  if (countries.length === 0) {
+    return;
+  }
+
+  if (countries.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+
+  countries.forEach(country => {
+    const { name, capital, population, flags, languages } = country;
+
+    const listItem = document.createElement('li');
+    const flagImage = document.createElement('img');
+    const countryName = document.createElement('h2');
+    const countryDetails = document.createElement('p');
+
+    flagImage.src = flags.svg;
+    flagImage.alt = `${name} Flag`;
+    countryName.textContent = name.official;
+    countryDetails.innerHTML = `
+      <strong>Capital:</strong> ${capital}<br>
+      <strong>Population:</strong> ${population}<br>
+      <strong>Languages:</strong> ${languages.map(lang => lang.name).join(', ')}
+    `;
+
+    listItem.appendChild(flagImage);
+    listItem.appendChild(countryName);
+    listItem.appendChild(countryDetails);
+
+    countryList.appendChild(listItem);
+  });
+}
+
+function showCountryNotFoundError() {
+  Notify.failure('Oops, there is no country with that name.');
+}
+
+const handleSearch = debounce(() => {
+  const searchTerm = countryInput.value.trim();
+
+  if (searchTerm !== '') {
+    fetchCountriesData(searchTerm)
+      .then(countries => {
+        displayCountries(countries);
+      })
+      .catch(error => {
+        if (error.message === 'Country not found') {
+          showCountryNotFoundError();
+        } else {
+          console.log('An error occurred:', error);
+        }
+      });
+  } else {
+    countryList.innerHTML = '';
+  }
+});
+
+const debounce_delay = 300;
+
+countryInput.addEventListener('input', debounce(handleSearch, debounce_delay));
