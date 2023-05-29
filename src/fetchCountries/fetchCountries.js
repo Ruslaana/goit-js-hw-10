5b60446ff1a5bf356c28b73b61aa74f67ad6589a
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-05-29 18:40:30 +0200

    fetch correct

--- src/fetchCountries/fetchCountries.js
+++ src/fetchCountries/fetchCountries.js
@@ -3,20 +3,12 @@ const URL_API = 'https://restcountries.com/v3.1/name';
 function fetchCountriesData(name) {
   return fetch(
     `${URL_API}/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Country not found');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (Array.isArray(data)) {
-        return data;
-      } else {
-        return [data];
-      }
-    });
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error('Country not found');
+    }
+    return response.json();
+  });
 }
 
 export { fetchCountriesData };
